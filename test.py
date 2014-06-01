d61538fdbfa1342c7aaf880016c97d7c78aa8aaa
Bugra Akyildiz <bugraa@live.com>
2014-05-31 22:38:36 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import unittest
+
+import angel
+import config
+
+MY_ID = '518840'
+VIACOM_ID = '40744'
+WHATSAPP_ID = '78902'
+UBER_ID = '19163'
+AIRBNN_ID = '32677'
+KARMA_ID = '29741'
+CB_INSIGHTS_ID = '344401'
+
+class AngelListTestCase(unittest.TestCase):
+
+  def __init__(self):
+
+
+  def set_up(self):
+    pass
+
+
+  def tear_down(self):
+    pass
+
+
+  def test_me(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
