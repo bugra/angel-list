d1b5c339707c56df29617bee4ae2b5841d65d855
Bugra Akyildiz <bugraa@live.com>
2014-05-31 23:53:05 -0400

    Two test cases

--- test.py
+++ test.py
@@ -11,9 +11,12 @@ AIRBNN_ID = '32677'
 KARMA_ID = '29741'
 CB_INSIGHTS_ID = '344401'
 
-class AngelListTestCase(unittest.TestCase):
+angel = angel.AngelList(config.CLIENT_ID,
+                        config.CLIENT_SECRET,
+                        config.ACCESS_TOKEN
+                       )
 
-  def __init__(self):
+class AngelListTestCase(unittest.TestCase):
 
 
   def set_up(self):
@@ -24,8 +27,24 @@ class AngelListTestCase(unittest.TestCase):
     pass
 
 
-  def test_me(self):
-    pass
+  def test_self(self):
+    self_ = angel.get_self()
+    assert self_['name'] == config.MY_NAME
+    assert self_['twitter_url'] == config.TWITTER_URL
+    assert self_['online_bio_url'] == config.ONLINE_BIO_URL
+    assert self_['linkedin_url'] == config.LINKEDIN_URL
+    assert self_['github_url'] == config.GITHUB_URL
+    assert self_['email'] == config.EMAIL
+    assert self_['angellist_url'] == config.ANGELLIST_URL
+    assert int(self_['id']) == config.ID
+
+  def test_search_for_slugs(self):
+    slug_ = angel.get_search_for_slugs('karma')
+    assert int(slug_['id']) == 29741
+    assert slug_['name'] == 'Karma'
+    assert slug_['type'] == 'Startup'
+    assert slug_['url'] == 'https://angel.co/karma'
+
 
 if __name__ == '__main__':
   unittest.main()
