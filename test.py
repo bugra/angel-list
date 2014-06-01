0d6e7d983258c950b314355afb3526128a4a2281
Bugra Akyildiz <bugraa@live.com>
2014-06-01 19:26:38 -0400

    :flushed:

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import angel
 # Put your credentials into config.py
 # in the following format
 """
-CLIENT_ID = '8678157397d4e21c6c6bed7a0c0fa366'
-CLIENT_SECRET = '01a0deb42173a19122291b08247f57f2'
-ACCESS_TOKEN = 'b0cf040043612cc40c5e37478354ebdc'
+CLIENT_ID =
+CLIENT_SECRET =
+ACCESS_TOKEN = 
 
 ### If you want to test your profile (test_self())
 ### Fill the credentials with the information
