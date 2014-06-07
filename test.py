7caad09092561593f89117dee984c24fad104efc
Bugra Akyildiz <bugraa@live.com>
2014-06-06 23:33:30 -0400

    Feed tests are completed

--- test.py
+++ test.py
@@ -405,6 +405,33 @@ class AngelListTestCase(unittest.TestCase):
     self.assertEqual(type(f_elem['likes']), int)
     self.assertEqual(type(f_elem['actor']), dict)
     self.assertEqual(type(f_elem['item']), dict)
+    f_ = angel.get_feeds(personalized=True)
+    expected_keys = sorted(['feed', 'per_page', 'last_page', 'total', 'page'])
+    self.assertEqual(sorted(list(f_.iterkeys())), expected_keys)
+    f_elem = f_['feed'][0]
+    e_ = sorted(['description', 'extra', 'timestamp', 'comments', 'actor', 'item', 'likes', 'text', 'promoted_by', 'id', 'target'])
+    self.assertEqual(e_, sorted(list(f_elem.iterkeys())))
+    self.assertEqual(type(f_elem['description']), unicode)
+    self.assertEqual(type(f_elem['timestamp']), unicode)
+    self.assertEqual(type(f_elem['id']), unicode)
+    self.assertEqual(type(f_elem['comments']), int)
+    self.assertEqual(type(f_elem['likes']), int)
+    self.assertEqual(type(f_elem['actor']), dict)
+    self.assertEqual(type(f_elem['item']), dict)
+    epoch = '1388890909' # May 1 2014
+    f_ = angel.get_feeds(since=epoch)
+    expected_keys = sorted(['feed', 'cursor',])
+    self.assertEqual(sorted(list(f_.iterkeys())), expected_keys)
+    f_elem = f_['feed'][0]
+    e_ = sorted(['description', 'extra', 'timestamp', 'comments', 'actor', 'item', 'likes', 'text', 'promoted_by', 'id', 'target'])
+    self.assertEqual(e_, sorted(list(f_elem.iterkeys())))
+    self.assertEqual(type(f_elem['description']), unicode)
+    self.assertEqual(type(f_elem['timestamp']), unicode)
+    self.assertEqual(type(f_elem['id']), unicode)
+    self.assertEqual(type(f_elem['comments']), int)
+    self.assertEqual(type(f_elem['likes']), int)
+    self.assertEqual(type(f_elem['actor']), dict)
+    self.assertEqual(type(f_elem['item']), dict)
 
 
 if __name__ == '__main__':
