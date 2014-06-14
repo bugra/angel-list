d43f9a5348ea4f18de109eee2f2273129fe3f1f6
Bugra Akyildiz <bugraa@live.com>
2014-06-14 15:18:38 -0400

    Add the credentials :poop: for travis

--- angel/config.py
+++ angel/config.py
@@ -1,6 +1,6 @@
-#CLIENT_ID = #
-#CLIENT_SECRET =
-#ACCESS_TOKEN =
+CLIENT_ID = '8678157397d4e21c6c6bed7a0c0fa366'
+CLIENT_SECRET = '01a0deb42173a19122291b08247f57f2'
+ACCESS_TOKEN = 'b0cf040043612cc40c5e37478354ebdc'
 
 
 # Change the following credentials to your account
