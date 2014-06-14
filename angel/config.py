9f866001d936cce353be7efd8fadf02674fe6b1e
Bugra Akyildiz <bugraa@live.com>
2014-06-14 14:57:07 -0400

    Credentials for test in travis

---
+++ angel/config.py
@@ -0,0 +1,13 @@
+CLIENT_ID = 'YOURCLIENT_ID'
+CLIENT_SECRET = 'YOURCLIENT_SECRET'
+ACCESS_TOKEN = 'YOURACCESS_TOKEN'
+
+# Change the following credentials to your account
+MY_NAME = 'Bugra Akyildiz'
+TWITTER_URL = 'https://twitter.com/bugraa'
+ONLINE_BIO_URL = 'http://bugra.github.io/'
+LINKEDIN_URL = 'http://www.linkedin.com/in/bugraa'
+GITHUB_URL = 'https://github.com/bugra'
+EMAIL = 'bakyildiz@cbinsights.com'
+ANGELLIST_URL = 'https://angel.co/bugra-akyildiz'
+ID = 518440
